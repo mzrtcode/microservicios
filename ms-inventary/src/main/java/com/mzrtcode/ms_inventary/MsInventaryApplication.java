749881713236ddab84e5962873fdc05d0a377dcc
Stiven Medina <stvnm33@gmail.com>
2024-08-04 22:43:01 -0500

    Crear proyecto base

---
+++ ms-inventary/src/main/java/com/mzrtcode/ms_inventary/MsInventaryApplication.java
@@ -0,0 +1,13 @@
+package com.mzrtcode.ms_inventary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInventaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInventaryApplication.class, args);
+	}
+
+}
---
+++ ms-inventary/src/test/java/com/mzrtcode/ms_inventary/MsInventaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzrtcode.ms_inventary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInventaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
