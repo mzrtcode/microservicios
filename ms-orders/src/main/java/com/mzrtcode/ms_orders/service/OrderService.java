1d8ecaa1bf1628a0c9c5c793e7a4c1c3f22319e4
Stiven Medina <stvnm33@gmail.com>
2024-08-06 10:11:31 -0500

    Configurar WebClient para ms-orders

--- ms-orders/src/main/java/com/mzrtcode/ms_orders/service/OrderService.java
+++ ms-orders/src/main/java/com/mzrtcode/ms_orders/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.mzrtcode.ms_orders.service;
 
 import com.mzrtcode.ms_orders.mapper.OrderMapper;
+import com.mzrtcode.ms_orders.model.dto.BaseResponse;
 import com.mzrtcode.ms_orders.model.dto.OrderRequest;
 import com.mzrtcode.ms_orders.model.dto.OrderResponse;
 import com.mzrtcode.ms_orders.model.entity.Order;
@@ -8,7 +9,7 @@ import com.mzrtcode.ms_orders.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.resource.ResourceUrlProvider;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
 
@@ -19,16 +20,32 @@ public class OrderService {
 
     private final OrderMapper orderMapper;
     private final OrderRepository orderRepository;
-    private final ResourceUrlProvider mvcResourceUrlProvider;
+    private final WebClient.Builder webClientBuilder;
 
 
     public OrderResponse placeOrder(OrderRequest orderRequest) {
-        Order order = orderMapper.toOrder(orderRequest);
-        order.setOrderNumber(UUID.randomUUID().toString());
 
         //TODO: Check products in inventary
-        Order savedOrder = orderRepository.save(order);
-        log.info("Order saved: {}", order);
-        return orderMapper.toOrderResponse(savedOrder);
+        BaseResponse result = this.webClientBuilder.build()
+                .post()
+                .uri("http://localhost:8083/api/inventary/in-stock")
+                .bodyValue(orderRequest.getOrderItems())
+                .retrieve()
+                .bodyToMono(BaseResponse.class)
+                .block();
+
+        if(result != null && !result.hasErrors()){
+            Order order = orderMapper.toOrder(orderRequest);
+            order.setOrderNumber(UUID.randomUUID().toString());
+
+            Order savedOrder = orderRepository.save(order);
+            log.info("Order saved: {}", order);
+            return orderMapper.toOrderResponse(savedOrder);
+        }else{
+            throw new IllegalArgumentException("Some of the products are not in stock");
+        }
+
+
+
     }
 }
