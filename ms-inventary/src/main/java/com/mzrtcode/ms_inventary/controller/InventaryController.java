18476cd16ed635574eafecad0d4b29db5e74ded8
Stiven Medina <stvnm33@gmail.com>
2024-08-07 17:49:44 -0500

    Corregir endpoint del ms-inventary

--- ms-inventary/src/main/java/com/mzrtcode/ms_inventary/controller/InventaryController.java
+++ ms-inventary/src/main/java/com/mzrtcode/ms_inventary/controller/InventaryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/inventary")
+@RequestMapping("/api/inventory")
 @RequiredArgsConstructor
 public class InventaryController {
 
